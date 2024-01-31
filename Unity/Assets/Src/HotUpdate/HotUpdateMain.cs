7e1a84218d6859f2cf6eea2e5f532dc472cace0a
QinMo <994674480@qq.com>
2024-01-31 17:32:33 +0800

    <Inti> 初始化项目

---
+++ Unity/Assets/Src/HotUpdate/HotUpdateMain.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class HotUpdateMain 
+{
+    public static void Start()
+    {
+        Debug.Log("HotUpdateMain Start");
+    }
+}
---
+++ Unity/Assets/Src/Mian.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mian : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadDll.Init();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
