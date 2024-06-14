288873473284676901eccec5e833cc102aeed73f
QinMo <994674480@qq.com>
2024-06-14 17:58:05 +0800

    feat：资源管理

---
+++ Unity/Assets/Src/AOT/Framework/1_Resource/ResourceModule.Data.cs
@@ -0,0 +1,14 @@
+﻿using Framework;
+
+namespace Src.AOT.Framework.Resource
+{
+    public partial class ResourceManager:IGameModule
+    {
+        internal enum ResourceRunningMode
+        {
+            EDITOR, //在编辑器下，不需要构建资源包，来模拟运行游戏
+            STANDALONE, //构建资源包使用单机运行模式
+            ONLINE, //构建热更新资源包使用联机模式运行
+        }
+    }
+}
--- Unity/Assets/Src/AOT/Framework/1_Resource/ResourceModule.cs
+++ Unity/Assets/Src/AOT/Framework/1_Resource/ResourceModule.cs
@@ -1,10 +1,14 @@
+using Cysharp.Threading.Tasks;
 using Framework;
+using UnityEngine;
 using YooAsset;
+
 
 namespace Src.AOT.Framework.Resource
 {
-    public class ResourceManager:IGameModule
+    public partial class ResourceManager:IGameModule
     {
+        internal ResourceRunningMode Mode = ResourceRunningMode.EDITOR; 
         public uint Priority { get => 1; }
         public void Init()
         {
@@ -14,7 +18,70 @@ namespace Src.AOT.Framework.Resource
             var package = YooAssets.CreatePackage("DefaultPackage");
             // 设置该资源包为默认的资源包，可以使用YooAssets相关加载接口加载该资源包内容。
             YooAssets.SetDefaultPackage(package);
+            if (Mode == ResourceRunningMode.STANDALONE)
+            {
+                InitializeYooAssetForStandaloneMode(package);
+            }
+            else if (Mode == ResourceRunningMode.ONLINE)
+            {
+                InitializeYooAsset(package);
+            }
+#if UNITY_EDITOR
+            else if(Mode == ResourceRunningMode.EDITOR)
+            {
+                InitializeYooAssetForEditorMode(package);
+            }
+#endif
         }
+
+#if UNITY_EDITOR
+        /// <summary>
+        /// 编辑器模式下运行
+        /// </summary>
+        /// <param name="package"></param>
+        /// <returns></returns>
+        private UniTask InitializeYooAssetForEditorMode(ResourcePackage package)
+        {
+            var initParameters = new EditorSimulateModeParameters();
+            var simulateManifestFilePath = EditorSimulateModeHelper.SimulateBuild(EDefaultBuildPipeline.BuiltinBuildPipeline, "DefaultPackage");
+            initParameters.SimulateManifestFilePath  = simulateManifestFilePath;
+            return package.InitializeAsync(initParameters).ToUniTask();
+        } 
+#endif
+
+        /// <summary>
+        /// 单机模式运行，需要构建资源包
+        /// </summary>
+        /// <param name="package"></param>
+        /// <returns></returns>
+        private UniTask InitializeYooAssetForStandaloneMode(ResourcePackage package)
+        {
+            var initParameters = new OfflinePlayModeParameters();
+            return package.InitializeAsync(initParameters).ToUniTask();
+        }
+        
+        private UniTask InitializeYooAsset(ResourcePackage package)
+        {
+            // 注意：GameQueryServices.cs 太空战机的脚本类，详细见StreamingAssetsHelper.cs
+            // string defaultHostServer = "http://127.0.0.1/CDN/Android/v1.0";
+            // string fallbackHostServer = "http://127.0.0.1/CDN/Android/v1.0";
+            // var initParameters = new HostPlayModeParameters();
+            // initParameters.BuildinQueryServices = new GameQueryServices(); 
+            // initParameters.DecryptionServices = new FileOffsetDecryption();
+            // initParameters.RemoteServices = new RemoteServices(defaultHostServer, fallbackHostServer);
+            // var initOperation = package.InitializeAsync(initParameters);
+            // return initOperation;
+            //
+            // if(initOperation.Status == EOperationStatus.Succeed)
+            // {
+            //     Debug.Log("资源包初始化成功！");
+            // }
+            // else 
+            // {
+            //     Debug.LogError($"资源包初始化失败：{initOperation.Error}");
+            // }
+        }
+
 
         public void Entry()
         {
