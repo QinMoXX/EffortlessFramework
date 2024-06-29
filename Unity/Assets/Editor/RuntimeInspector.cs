ef353432c368b3dfa4b6a9aaa4cd79103d6a6a25
QinMo <994674480@qq.com>
2024-06-29 15:13:21 +0800

    feat：运行时代码检查器

--- Unity/Assets/Editor/RuntimeInspector.cs
+++ Unity/Assets/Editor/RuntimeInspector.cs
@@ -1,7 +1,71 @@
-namespace Editor
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Framework;
+using Src.AOT.Framework;
+using UnityEditor;
+using UnityEngine;
+
+/// <summary>
+/// 运行时代码检查器
+/// </summary>
+[InitializeOnLoad]
+public class RuntimeInspector
 {
-    public class RuntimeInspector
+    static RuntimeInspector()
     {
-        
+        DependencyTracker dependencyTracker = new DependencyTracker();
+        //获取AOT程序集，如有需要可以增加程序集
+        Assembly assembly = Assembly.GetAssembly(typeof(DependencyModuleAttribute));
+        dependencyTracker.TrackDependencies(assembly);
+        foreach (var type in dependencyTracker.GetDependencies().Keys)
+        {
+            if (!typeof(IGameModule).IsAssignableFrom(type))
+            {
+                throw new InvalidOperationException(
+                    $"The type '{type.Name}' must implement 'IGameModule' to use the DependencyModuleAttribute.");
+            }
+        }
+        Debug.Log("代码检查无误");
+    }
+    
+    
+}
+
+public class DependencyTracker
+{
+    private readonly Dictionary<Type, HashSet<Type>> _dependencies;
+
+    public DependencyTracker()
+    {
+        _dependencies = new Dictionary<Type, HashSet<Type>>();
+    }
+
+    public void TrackDependencies(Assembly assembly)
+    {
+        foreach (var type in assembly.GetTypes())
+        {
+            var dependencyAttributes = (DependencyModuleAttribute[])type.GetCustomAttributes(typeof(DependencyModuleAttribute), false);
+            if (dependencyAttributes.Length > 0)
+            {
+                foreach (var attribute in dependencyAttributes)
+                {
+                    foreach (var dependency in attribute.Dependencies)
+                    {
+                        if (!_dependencies.ContainsKey(type))
+                        {
+                            _dependencies[type] = new HashSet<Type>();
+                        }
+                        _dependencies[type].Add(dependency);
+                    }
+                }
+            }
+        }
+    }
+
+    public Dictionary<Type, HashSet<Type>> GetDependencies()
+    {
+        return _dependencies;
     }
 }
+
