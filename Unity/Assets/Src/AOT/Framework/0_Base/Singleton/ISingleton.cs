ebb256be4a0c51505e3dc9d5e00f2d5ae05b23a6
QinMo <994674480@qq.com>
2024-07-01 15:39:30 +0800

    feat：singleton

---
+++ Unity/Assets/Src/AOT/Framework/0_Base/Singleton/ISingleton.cs
@@ -0,0 +1,21 @@
+﻿
+namespace AOT.Framework
+{
+	public interface ISingleton
+	{
+		/// <summary>
+		/// 创建单例
+		/// </summary>
+		void OnCreate(System.Object createParam);
+
+		/// <summary>
+		/// 更新单例
+		/// </summary>
+		void OnUpdate();
+
+		/// <summary>
+		/// 销毁单例
+		/// </summary>
+		void OnDestroy();
+	}
+}
