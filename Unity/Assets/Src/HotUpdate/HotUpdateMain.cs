cdc3d273989a9b52446c15b1984ab9d304f88d56
QinMo <994674480@qq.com>
2024-07-02 18:02:01 +0800

    feat：数据表模块创建

--- Unity/Assets/Src/HotUpdate/HotUpdateMain.cs
+++ Unity/Assets/Src/HotUpdate/HotUpdateMain.cs
@@ -1,14 +1,18 @@
 using AOT.Framework;
 using AOT.Framework.Debug;
 using AOT.Framework.Procedure;
+using AOT.Framework.Resource;
+using HotUpdate;
 
 public class HotUpdateMain:IEntry
 {
     public void Entry()
     {
         EDebug.Log("热更新代码加载成功");
+        GameEntry.CreatModule<TableManager>().Initialize(GameEntry.GetModule<ResourceManager>());
         GameEntry.GetModule<ProcedureManager>().Initialize(typeof(HomeMenuProcedure));
         GameEntry.GetModule<ProcedureManager>().StartProcedure<HomeMenuProcedure>();
+        
     }
 
     public void Exit()
