3a1159c242a9fd5fb3612eb0f9bfa6ffcda09f78
QinMo <994674480@qq.com>
2024-08-07 15:07:03 +0800

    refactor：更好的双端项目结构

---
+++ Share/Share/PlayerDate.cs
@@ -0,0 +1,13 @@
+namespace Share
+{
+    [Serializable]
+    public struct PlayerData
+    {
+        public string id;
+        public float xPos;
+        public float yPos;
+        public float zPos;
+        public double timestamp;
+    }
+}
+
