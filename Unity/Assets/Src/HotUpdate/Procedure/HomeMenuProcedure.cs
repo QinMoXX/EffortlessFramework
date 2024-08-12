4ea6ad8c843cf865a3487edf475a9b4d7051be5d
QinMo <994674480@qq.com>
2024-08-12 20:37:22 +0800

    feat：主菜单流程

--- Unity/Assets/Src/HotUpdate/Procedure/HomeMenuProcedure.cs
+++ Unity/Assets/Src/HotUpdate/Procedure/HomeMenuProcedure.cs
@@ -1,16 +1,20 @@
-using System;
-using System.Threading.Tasks;
+using AOT.Environment;
 using AOT.Framework;
 using AOT.Framework.Debug;
 using AOT.Framework.Fsm;
+using AOT.Framework.Mvc;
 using AOT.Framework.Procedure;
+using AOT.Framework.Resource;
+using AOT.Framework.Scene;
 using Cysharp.Threading.Tasks;
 using HotUpdate.UI;
 using HotUpdate.UI.View;
 using HotUpdate.Utility;
+using UnityEngine;
 
 public class HomeMenuProcedure:ProcedureBase
 {
+    private bool enterMap;
     public HomeMenuProcedure(IFsm handle) : base(handle)
     {
     }
@@ -22,14 +26,28 @@ public class HomeMenuProcedure:ProcedureBase
 
     protected override void OnEnter()
     {
-        EDebug.Log("进入主菜单");
+        EDebug.Log("进入主菜单界面");
 
         OnUpdateAsync().Forget();
     }
 
     private async UniTaskVoid OnUpdateAsync()
     {
+        EnvCamera.MainCamera.transform.position = new Vector3(0, 0, -5);
+        EnvCamera.MainCamera.gameObject.AddComponent<Skybox>().material = 
+            await GameEntry.GetModule<ResourceManager>().LoadAssetAsync<Material>("Material_FS013_Day");
+        GameEntry.GetModule<SceneManager>().LoadSceneAsync("Home", default, OnSceneLoadedActin, OnSceneLoadingAction,false);
         await UIUtility.ShowAndTryCreateUIForm<LoginUI>(UIID.LoginUI);
+    }
+    
+    private void OnSceneLoadingAction(float progress, string sceNename)
+    {
+        EDebug.Log($"OnSceneLoadingAction progress: {progress.ToString()}");
+    }
+
+    private void OnSceneLoadedActin(bool isSuccess, string sceNename)
+    {
+        GameEntry.GetModule<SceneManager>().UnloadScene("Main");
     }
 
     protected override void OnUpdate(float elapseSeconds, float realElapseSeconds)
