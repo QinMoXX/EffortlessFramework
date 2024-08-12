d96766259a945b8df5d039787040c1e80d592c73
QinMo <994674480@qq.com>
2024-08-12 23:43:03 +0800

    feat：事件组

---
+++ Unity/Assets/Src/HotUpdate/Event/EventGroup.cs
@@ -0,0 +1,14 @@
+using AOT.Framework.Event;
+
+namespace HotUpdate.Event
+{
+    /// <summary>
+    /// UI事件组
+    /// </summary>
+    public sealed class UIEventGroup:EventBasis { }
+    
+    /// <summary>
+    /// 世界事件组
+    /// </summary>
+    public sealed class WorldEventGroup:EventBasis { }
+}
