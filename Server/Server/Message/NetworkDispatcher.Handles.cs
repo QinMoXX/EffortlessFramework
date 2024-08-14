9c33b6ac30e1fba6949d1ce4058dce8cd7d7681c
QinMo <994674480@qq.com>
2024-08-14 11:18:01 +0800

    feat：服务端进入大厅 进入房间

--- Server/Server/Message/NetworkDispatcher.Handles.cs
+++ Server/Server/Message/NetworkDispatcher.Handles.cs
@@ -1,6 +1,7 @@
 using AOT.Framework.Network;
 using HotUpdate.Network.Message;
 using MongoDB.Driver;
+using Server.Core;
 using Server.DBModel;
 using Server.Service;
 
@@ -20,11 +21,14 @@ namespace HotUpdate.Network
                 {
                     session.SendMessage<ResLogin>((int)NetworkMessageIds.ResLogin, new ResLogin
                     {
-                        result = true, token = session.SessionId.ToString()
+                        result = true, token = session.GetToken(),userId = result.UID
                     });
                     var update = Builders<User>.Update.Set(user => user.LastLoginTime, DateTimeOffset.UtcNow.ToUnixTimeMilliseconds());
                     DBService.Instance.UpdateDataAsync<User>("User", filter, update);
-
+                    //登录成功
+                    Avatar avatar = Avatar.Create(session as NetSession, result);
+                    SessionManage.Instance.AddAvatar(session.SessionId, avatar);
+                    HallsService.Instance.JoinAvatar(avatar);
                 }
                 else
                 {
@@ -37,6 +41,45 @@ namespace HotUpdate.Network
             catch (Exception e)
             {
                 Console.WriteLine("OnReqLogin Failed! "+ e);
+            }
+
+        }
+
+        public static void OnReqEntryRoom(INetSession session, ReqEntryRoom reqEntryRoom)
+        {
+            if (reqEntryRoom == null)
+            {
+                return;
+            }
+            Console.WriteLine($"OnReqEntryRoom: {reqEntryRoom.token}");
+            if (session.GetToken() != reqEntryRoom.token)
+            {
+                Console.WriteLine("OnReqEntryRoom Failed! Token is invalid!");
+            }
+
+            Avatar avatar = SessionManage.Instance.GetAvatar(session.SessionId);
+
+            if (avatar == null)
+            {
+                Console.WriteLine("OnReqEntryRoom Failed! Avatar is not exist in Halls!");
+                return;
+            }
+
+            //离开大厅
+            HallsService.Instance.LeaveAvatar(avatar);
+            try
+            {
+                var room = HallsService.Instance.EntryRoom(avatar);
+                session.SendMessage<ResEntryRoom>((int)NetworkMessageIds.ResEntryRoom, new ResEntryRoom()
+                {
+                    roomId = room.roomId,
+                    result = true,
+                });
+            }
+            catch (Exception e)
+            {
+                // 如果出现进入房间失败，则返回大厅
+                HallsService.Instance.JoinAvatar(avatar);
             }
 
         }
